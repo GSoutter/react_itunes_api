779be943b20fe9069996e2846144422eeb57018a
Gordon Soutter <gordon.soutter@outlook.com>
2020-06-16 18:44:32 +0100

    adds api fetch to itunes top 20 songs

--- src/containers/SongsContainer.js
+++ src/containers/SongsContainer.js
@@ -11,6 +11,13 @@ class SongsContainer extends Component {
 
   }
 
+  componentDidMount(){
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json"
+    fetch(url)
+    .then(res => res.json())
+    .then(jsonRes => this.setState({songs: jsonRes.feed.entry}))
+  }
+
   render() {
     return (
       <div>Hello from SongsContainer</div>
